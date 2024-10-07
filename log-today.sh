2fe406fbf384d2a5b3455c6d7c8c3cc0f607e5ec
Kristof Havasi <havasi@efr.de>
2024-10-07 17:59:03 +0200

    Fix loguru level

--- log-today.sh
+++ log-today.sh
@@ -2,7 +2,7 @@
 
 export TOGGL_PASS="$(pass show toggl-pass | tr -d '\n')"
 export PERSO_PASS="$(pass show personio-pass | tr -d '\n')"
-export LOGURU_LEVEL='info'
+export LOGURU_LEVEL='INFO'
 
 SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd $SCRIPTDIR
